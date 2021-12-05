4d77b085bfdb7dc90e68948ce545c2252cca4d8b
unknown <fatemehbaghbani87@gmail.com  git config --global user.name fatemehfb99>
2021-12-05 08:09:52 +0330

    pythom

--- 5-3.py
+++ 5-3.py
@@ -4,5 +4,10 @@ for i in range (n):
     a=int(input("please enter a number : "))
     arr.append(a)
 print(arr)
-arr.sort()
+for i in range (n-1):
+    for j in range (n-1):
+        if arr[j]>arr[j+1]:
+            temp=arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
 print(arr)
